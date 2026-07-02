551c628a95a8541ceb460d2a6b88b6efa0d2ca9d
Andre Lucas Trevizan <andre.trevizan@tirol.com.br>
2026-07-02 10:00:17 -0300

    Feat: Remoção Nginx

--- web/next.config.ts
+++ web/next.config.ts
@@ -1,8 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: "standalone",
-  basePath: "/abatimentos"
+  output: "standalone"
 };
 
 export default nextConfig;
